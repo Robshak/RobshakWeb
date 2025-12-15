8da855c1f19fb5a6216371887fd94d0cfd454d04
Robshak <rdshakura@gmail.com>
2025-12-15 22:43:33 +0300

    change text in transition section

--- src/widgets/About/ui/Transition/Transition.tsx
+++ src/widgets/About/ui/Transition/Transition.tsx
@@ -18,9 +18,8 @@ export const Transition: FC<TTransitionProps> = (props) => {
         <img src={Avatar} alt="Avatar" className={s.avatar} />
         <Text view={'body'}>
           Although I work as a software engineer, I have many hobbies — most of them creative. I
-          play table tennis, write music, love movies, enjoy dancing, create videos, and dream of
-          writing books. Each of these passions is a part of me. Below, you’ll find a glimpse into
-          each of them.
+          play table tennis, write music, love movies, enjoy dancing, and dream of creating stories.
+          Each of these passions is a part of me. Below, you’ll find a glimpse into each of them.
         </Text>
       </div>
     </div>
